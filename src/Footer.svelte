d9331d50f0c32d1796ebbf53815ec46d89bc22ea
joseph0819 <umunnajoseph70@gmail.com>
2025-09-25 13:59:11 -0400

    new footer edit

--- src/Footer.svelte
+++ src/Footer.svelte
@@ -9,7 +9,7 @@
     <p class="font-semibold">🌟 Keep going, you’re doing great!</p>
 
     <p class="text-xs opacity-80">
-      © {year} TRackApp
+      © {year} TrackApp
     </p>
   </div>
 </footer>
